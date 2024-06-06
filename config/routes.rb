e7e75ad6b46b85e73a67c8a4fcdbdf72d1fd3779
Dhev <github@dhev.anonaddy.me>
2024-06-06 16:12:23 +0530

    Access Member Details

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  get '/member_details' => "members#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
