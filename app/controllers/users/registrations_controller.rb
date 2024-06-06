0d3320b8e701845da220dfadaeae6086e71a4487
Dhev <github@dhev.anonaddy.me>
2024-06-06 16:06:04 +0530

    Setup Sign in and Sign out

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,59 +4,51 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  respond_to :json
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+    resource.save
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        respond_with(resource)
+      else
+        expire_data_after_sign_in!
+        respond_with(resource, location: after_inactive_sign_up_path_for(resource))
+      end
+    else
+      clean_up_passwords(resource)
+      set_minimum_password_length
+      respond_with(resource)
+    end
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  private
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {
+          code: 200,
+          message: 'Signed Up Successfully',
+          data: resource
+        }
+      }, status: :ok
+    else
+      render json: {
+        status: {
+          message: 'User could not be created successfully',
+          errors: resource.errors.full_messages
+        }
+      }, status: :unprocessable_entity
+    end
+  end
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
